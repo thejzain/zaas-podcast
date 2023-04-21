5036f381498f038d31dd5c823af4574db8d3a675
Zain <thejzainkp@gmail.com>
2023-04-22 01:47:03 +0530

    login wireframe

--- pages/login/index.js
+++ pages/login/index.js
@@ -6,6 +6,9 @@ export default function LoginPage() {
   const pb = new PocketBase("http://127.0.0.1:8090");
   const email = useRef(0);
   const password = useRef(0);
+  const passwordConfirm = useRef(0);
+  const username = useRef(0);
+  const name = useRef(0);
 
   async function userLogin() {
     try {
@@ -22,38 +25,38 @@ export default function LoginPage() {
 
   async function userCreate() {
     const data = {
-      username: "test_username",
+      username: username.current.value,
       email: email.current.value,
       emailVisibility: true,
       password: password.current.value,
-      passwordConfirm: password.current.value,
-      name: "test",
+      passwordConfirm: passwordConfirm.current.value,
+      name: name.current.value,
     };
     const record = await pb.collection("users").create(data);
   }
   return (
-    <div>
-      <div className="grid justify-center">
+    <div className="bg-gradient-to-tr from-[#141413] to-[#2A2B2A] h-screen justify-center text-white">
+      <div className="grid justify-center border rounded-2xl p-11 w-96">
         <div className="grid grid-cols-2 text-center">
           <button onClick={() => setLoginpage(true)}>Login</button>
           <button onClick={() => setLoginpage(false)}>Register</button>
         </div>
         {loginpage ? (
-          <div>
-            <div>
+          <div className="">
+            <div className="m-5">
               <input
                 type="text"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="email"
                 ref={email}
-                className="border-2"
-                placeholder="Email"
               ></input>
             </div>
-            <div>
+            <div className="m-5">
               <input
                 type="password"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="password"
                 ref={password}
-                className="border-2"
-                placeholder="Password"
               ></input>
             </div>
             <div className="grid place-content-center">
@@ -62,7 +65,49 @@ export default function LoginPage() {
           </div>
         ) : (
           <div>
-            <div>ok</div>
+            <div className="m-5">
+              <input
+                type="text"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="username"
+                ref={username}
+              ></input>
+            </div>
+            <div className="m-5">
+              <input
+                type="text"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="Name"
+                ref={name}
+              ></input>
+            </div>
+            <div className="m-5">
+              <input
+                type="text"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="email"
+                ref={email}
+              ></input>
+            </div>
+            <div className="m-5">
+              <input
+                type="password"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="password"
+                ref={password}
+              ></input>
+            </div>
+            <div className="m-5">
+              <input
+                type="password"
+                className="p-2 text-black rounded-2xl opacity-75 "
+                placeholder="confirm passwoord"
+                ref={passwordConfirm}
+              ></input>
+            </div>
+            <div className="grid justify-center">
+                <button type="submit" className="bg-slate-700 px-5 py-2 rounded-2xl" onClick={userCreate}>Register</button>
+            </div>
           </div>
         )}
       </div>
