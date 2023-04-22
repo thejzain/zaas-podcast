afb2792b86415781157d020a2f11edccdbf2ae84
Agastus Joyson <agastusjoyson@gmail.com>
2023-04-22 12:19:45 +0530

    Set carousel

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -10,21 +10,29 @@ function Layout({children}) {
     const [visible,setVisible] = useState(false);
 
     return(
-        <div className='min-h-screen p-6 bg-white'>
-            <header className='flex gap-4 w-full h-10'>
-                <button className='sm:hidden' onClick={() => setVisible(true)}><span className='pi pi-arrow-right'></span></button>
-                <div>Listen Now</div>
-                <div className='ml-auto'>User's name</div>
-                <img src='' alt='user_image'></img>
-            </header>
-            <main>{children}</main>
-            <Sidebar visible={visible} position='left' onHide={() => setVisible(false)}>
-                <h2>Music</h2>
-                <Link href="/" onClick={()=> setVisible(false)}>Home</Link>
-                <div>Listen Now</div>
-                <div>Browse</div>
-                <div>Search</div>
-                <Link href="/test" onClick={() => setVisible(false)}>Test page</Link>
+        <div className='min-h-screen p-5 bg-white sm:grid sm:grid-cols-[1fr_3fr]'>
+            <div className='hidden sm:block'>
+                <div className='font-bold text-xl'>Music</div>
+                <div className='flex flex-col'>
+                    <Link href='/'>Home</Link>
+                    <Link href='/test'>Test page</Link>
+                </div>
+            </div>
+            <div>
+                <header className='flex gap-4'>
+                    <button className='sm:hidden' onClick={() => setVisible(true)}><span className='pi pi-bars'></span></button>
+                    <div className='font-extrabold text-xl'>Listen Now</div>
+                    <div className='ml-auto'>User's name</div>
+                    <img src='' alt='user_image'></img>
+                </header>
+                <main>{children}</main>
+            </div>
+            <Sidebar visible={visible} position='left' onHide={() => setVisible(false)} >
+                <h2 className='font-bold text-xl'>Music</h2>
+                <div className='flex flex-col'>
+                    <Link href="/" onClick={()=> setVisible(false)}>Home</Link>
+                    <Link href="/test" onClick={() => setVisible(false)}>Test page</Link>
+                </div>
             </Sidebar>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,71 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import {Sidebar} from 'primereact/sidebar'
+import 'primeicons/primeicons.css'
+import "primereact/resources/themes/lara-light-indigo/theme.css"
+import "primereact/resources/primereact.min.css"   
+import {Carousel} from 'primereact/carousel'
 import { useState } from 'react'
-// import 'primeicons/primeicons.css'
-// import "primereact/resources/themes/lara-light-indigo/theme.css"
-// import "primereact/resources/primereact.min.css"   
-import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  
-  //const [visible,setVisible] = useState(false);
+
+  const [items,setItems] = useState([
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 1',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 2',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 3',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 4',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 5',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 6',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 7',
+    },
+    {
+      image: 'https://picsum.photos/200',
+      title: 'Title 8',
+    },
+  ]);
+
+  const responsiveOptions = [
+    {
+      breakpoint: '640px',
+      numVisible: 2,
+      numScroll:1,
+    },
+  ];
+
+  const itemTemplate = (item) => {
+    return(
+      <div className='flex flex-col items-center mr-2' >
+        <img src={item.image} alt={item.title}></img>
+        <div>{item.title}</div>
+      </div>
+    );
+  }
 
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-6 bg-white">
-    // <main className='flex min-h-screen p-6 bg-white'>
-    //   <header className='flex gap-4 w-full h-10'>
-    //     <button className='sm:hidden' onClick={() => setVisible(true)}><span className='pi pi-arrow-right'></span></button>
-    //     <div>Listen Now</div>
-    //     <div className='ml-auto'>User's name</div>
-    //     <img src='' alt='user_image'></img>
-    //   </header>
-    //   <Sidebar visible={visible} position='left' onHide={() => setVisible(false)}>
-    //     <h2>Music</h2>
-    //     <div>Listen Now</div>
-    //     <div>Browse</div>
-    //     <div>Search</div>
-    //     <Link href="/test">Test page</Link>
-    //   </Sidebar>
-    // </main>
-    <div>Main page initial</div>
+    <div>
+      <div>Main page</div>
+      <Carousel value={items} numScroll={1} numVisible={5} responsiveOptions={responsiveOptions} itemTemplate={itemTemplate}/>
+    </div>
   )
 }
